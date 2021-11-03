ff66d2e5f303241fb6f3c3c3572a1d48a1bd5202
a0197391 <83972769+a0197391@users.noreply.github.com>
2021-11-03 16:30:16 +0800

    Add files via upload

---
+++ OLED with Variable resistance.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include "SSD1306Wire.h"
+SSD1306Wire  display(0x3c, 21, 22);
+
+void setup() {
+    Serial.begin(115200);
+    // Initialising the UI will init the display too.
+    display.init();
+    display.flipScreenVertically(); //畫面上下顛倒顯示
+    display.clear(); //清空畫面
+    display.setFont(ArialMT_Plain_16);   //設定字型與大小
+    pinMode(12,INPUT);
+    
+}
+
+void loop() {
+  display.clear();  
+  int input=analogRead(12);
+  String myString=String(input);
+  display.drawString(0,5,myString);
+  display.display(); //顯示畫面
+  Serial.println(input);
+}
+
